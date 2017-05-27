0f1d3d6a3c24308ae434b2b4e4d35426db8bdebe
Kyle Gregory <kjgregory83@gmail.com>
2017-05-26 22:04:25 -0400

    added params for 256KB devices

--- 100_Projects_in_100_Days/Day029_BLE_SFlash_Write/User_SFlash_Write/User_SFlash_Write.cydsn/WriteUserSFlash.h
+++ 100_Projects_in_100_Days/Day029_BLE_SFlash_Write/User_SFlash_Write/User_SFlash_Write.cydsn/WriteUserSFlash.h
@@ -32,13 +32,15 @@
 *                  Enums and macros
 *****************************************************/  
 #define SWITCH_PRESSED                  (0u)   /* Active low user switch on BLE Pioneer kit */ 
-#define USER_SFLASH_ROW_SIZE            (128u) /* SFlash row size for 128KB flash BLE device. For other PSoC 4 BLE devices 
+#define USER_SFLASH_ROW_SIZE            (128u) /* SFlash row size for 128KB flash BLE device. For 256KB flash BLE device, 
+                                                * use (256u) for the row size. For other PSoC 4 BLE devices 
                                                 * with higher flash size, this example project might need some modification.
                                                 * Please check the device datasheet and TRM before using this code on non 128KB
                                                 * flash devices */
 #define SFLASH_STARTING_VALUE           (0x00) /* Starting value to be stored in user SFlash to demonstrate SFlash write API */
 #define USER_SFLASH_ROWS                (4u)   /* Total number of user SFlash rows supported by the device */
 #define USER_SFLASH_BASE_ADDRESS        (0x0FFFF200u) /* Starting address of user SFlash row for 128KB PSoC 4 BLE device */    
+                                               /* for 256KB PSoC4 device use 0x0FFFF400u base address */ 
     
 #define LOAD_FLASH				         0x80000004
 #define WRITE_USER_SFLASH_ROW	         0x80000018
