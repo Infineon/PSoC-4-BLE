4241eec6fca6f80fcf218c58a795d1041111b9b8
srikanthds <srikant.ds@gmail.com>
2018-03-01 00:24:42 +0530

    Updated to PSoC Creator 4.2

--- 100_Projects_in_100_Days/Day031_Digital_Sensor/PSoC_4_BLE_Pressure_Sensor/PSoC_4_BLE_Pressure_Sensor.cydsn/main.c
+++ 100_Projects_in_100_Days/Day031_Digital_Sensor/PSoC_4_BLE_Pressure_Sensor/PSoC_4_BLE_Pressure_Sensor.cydsn/main.c
@@ -1,42 +1,42 @@
 /************************************************************************************************************************
 * Project Name		: PSoC_4_BLE_Pressure_Sensor
 * File Name			: main.c
-* Version 			: 1.0
-* Device Used		: CY8C4247LQI-BL483
-* Software Used		: PSoC Creator 3.1
-* Compiler    		: ARM GCC 4.8.4
+* Version 			: 1.10
+* Device Used		: CY8C4248LQI-BL583
+* Software Used		: PSoC Creator 4.2
 * Related Hardware	: CY8CKIT-042-BLE Bluetooth Low Energy Pioneer Kit
 *************************************************************************************************************************/
 
 /************************************************************************************************************************
-* Copyright (2015), Cypress Semiconductor Corporation. All Rights Reserved.
+* Copyright (2015-2018), Cypress Semiconductor Corporation. All Rights Reserved.
 ************************************************************************************************************************
 * This software, including source code, documentation and related materials
-* ("Software") is owned by Cypress Semiconductor Corporation (Cypress) and is
-* protected by and subject to worldwide patent protection (United States and 
-* foreign), United States copyright laws and international treaty provisions. 
-* Cypress hereby grants to licensee a personal, non-exclusive, non-transferable
-* license to copy, use, modify, create derivative works of, and compile the 
-* Cypress source code and derivative works for the sole purpose of creating 
-* custom software in support of licensee product, such licensee product to be
-* used only in conjunction with Cypress's integrated circuit as specified in the
-* applicable agreement. Any reproduction, modification, translation, compilation,
-* or representation of this Software except as specified above is prohibited 
-* without the express written permission of Cypress.
-* 
+* (“Software”), is owned by Cypress Semiconductor Corporation or one of its
+* subsidiaries (“Cypress”) and is protected by and subject to worldwide patent
+* protection (United States and foreign), United States copyright laws and
+* international treaty provisions. Therefore, you may use this Software only
+* as provided in the license agreement accompanying the software package from
+* which you obtained this Software (“EULA”).
+*
+* If no EULA applies, Cypress hereby grants you a personal, nonexclusive,
+* non-transferable license to copy, modify, and compile the Software source
+* code solely for use in connection with Cypress’s integrated circuit products.
+* Any reproduction, modification, translation, compilation, or representation
+* of this Software except as specified above is prohibited without the express
+* written permission of Cypress.
+*
 * Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND, 
 * EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED 
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-* Cypress reserves the right to make changes to the Software without notice. 
-* Cypress does not assume any liability arising out of the application or use
-* of Software or any product or circuit described in the Software. Cypress does
-* not authorize its products for use as critical components in any products 
-* where a malfunction or failure may reasonably be expected to result in 
-* significant injury or death ("ACTIVE Risk Product"). By including Cypress's 
-* product in a ACTIVE Risk Product, the manufacturer of such system or application
-* assumes all risk of such use and in doing so indemnifies Cypress against all
-* liability. Use of this Software may be limited by and subject to the applicable
-* Cypress software license agreement.
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress 
+* reserves the right to make changes to the Software without notice. Cypress 
+* does not assume any liability arising out of the application or use of the 
+* Software or any product or circuit described in the Software. Cypress does 
+* not authorize its products for use in any products where a malfunction or 
+* failure of the Cypress product may reasonably be expected to result in 
+* significant property damage, injury or death (“High Risk Product”). By 
+* including Cypress’s product in a High Risk Product, the manufacturer of such 
+* system or application assumes all risk of such use and in doing so agrees to 
+* indemnify Cypress against all liability.
 ***********************************************************************************************************************/
 
 /************************************************************************************************************************
@@ -67,7 +67,22 @@
 * Sensor VIN - P3[4] - J2.7
 ************************************************************************************************************************/
 
-#include <main.h>
+#include <common.h>
+#include <BLEApplication.h>
+#include <I2CMaster.h>
+
+/*************************Macro Definitions**********************************/
+#define LED_DELAY_COUNT 0x32 //Counter value for LED Delay
+#define NOTIFICATION_DELAY_COUNT 0x20 //Counter value for Notification Delay
+/*****************************************************************************/
+
+/*************************Function Prototypes*******************************/
+void InitializeSystem(void);
+void ReadSensorDataAndNotify(void);
+void HandleStatusLED(void);
+void EnterExitLowPowerMode(void);
+CY_ISR_PROTO(ButtonISR);
+/*****************************************************************************/
 
 /*************************Variables Declaration*************************************************************************/
 uint8 ChangePowerPinDriveMode = TRUE; //This flag is set when the GATT disconnect event occurs
